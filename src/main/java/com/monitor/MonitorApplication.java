1752c01636a20ddde6105b1ccbfa535e66511faf
chitralimbu <limbu_nikkil@hotmail.co.uk>
2020-01-03 14:11:55 +0000

    MonitorController added

--- src/main/java/com/monitor/MonitorApplication.java
+++ src/main/java/com/monitor/MonitorApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MonitorApplication {
-	//Hello M Kaye
 	public static void main(String[] args) {
 		SpringApplication.run(MonitorApplication.class, args);
 	}
-
 }
